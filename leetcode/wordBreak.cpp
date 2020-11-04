1bc8fcfae4f3f7013699d093dcff682e4bb34ba9
lunar <lunar_ubuntu@qq.com>
2020-11-04 23:02:03 +0800

    单词拆分II

--- leetcode/wordBreak.cpp
+++ leetcode/wordBreak.cpp
@@ -6,6 +6,8 @@
   > Location        : Shanghai
  **********************************************/
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 /*
@@ -23,7 +25,36 @@ using namespace std;
 class Solution {
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        
+        Node* root = new Node(' ', 0);
+        vector<vector<string>> dp(s.size(), vector<string>());
+        /* build dictionary tree */
+        for (string& word: wordDict) {
+            root->insert(word);
+        }
+        int len = s.size();
+        for (int i = 0; i < len; i++) {
+            int k = i;
+            while (k >= 0) {
+                string sub_s = s.substr(k, i-k+1);
+                int search = root->exist(sub_s);
+                if (search == 0) {
+                    break;
+                } else if (search == 1) {
+                    k--;
+                    continue;
+                } else {
+                     if (k < 1) {
+                        dp[i].emplace_back(sub_s);
+                    } else {
+                        for (string& words: dp[k-1]) {
+                            dp[i].emplace_back(words + " " + sub_s);
+                        }
+                    }
+                }
+                k--;
+            }
+        }
+        return dp[len-1];
     }
 
     /* Build a dictionary tree.
@@ -35,20 +66,101 @@ public:
 private:
     struct Node {
         char c;
-        int state; /* state is to indicate three states of a node: 0 means no such next, 1 means it's a character of a preffix, 2 means it's not only a character of a preffix, but also the last character of a preffix, 3 means it's a root node, root does not represent any character, it connects with all kinds of preffixes. */
-        Node* nexts[26];
-        static void insert(const string& pre) {
-            _insert(pre, 0);
+        int state; /* state is to indicate three states of a node: 1 means it's a character of a preffix, 2 means it's not only a character of a preffix, but also the last character of a preffix, 0 means it's a root node, root does not represent any character, it connects with all preffixes. */
+        Node* nexts[26]{NULL};
+
+        Node(char c, int state = 1):c(c), state(state) {
+            cout << "create a node " << c << endl;
+        }
+
+        void insert(const string& suf) {
+            _insert(suf);
+        }
+
+        /* check if the word suf exits in the dictionary tree */
+        int exist(const string& suf) {
+            cout << "checking " << suf << endl;
+            return _exist(this, suf, suf.size()-1);
         }
         private:
-        void _insert(const string& pre, int k) {
-            /* only the root node is allowed to insert a new preffix*/
-            if (state != 3) {
+        void _insert(const string& suf) {
+            /* only the root node is allowed to insert a new sufffix*/
+            if (state != 0) {
                 return ;
             }
-            
+            _traverse(this, suf, suf.size()-1);
+        }
 
+        void _traverse(Node* node, const string& suf, int k) {
+            char c = suf[k];
+            if (node->nexts[c - 'a'] == NULL) {
+                node->nexts[c - 'a'] = new Node(c);
+            }
+            if (k == 0) {
+                node->nexts[c - 'a']->state = 2;
+                return ;
+            } else {
+                _traverse(node->nexts[c - 'a'], suf, k-1);
+            }
+        }
+
+        /* 
+         * return 3 kinds of results.
+         * 0: No such suffix in the dictionary tree
+         * 1: suf is a part of a word in the dictionary tree.
+         * 2: suf is a word in the dictionary tree
+         */
+        int _exist(Node* node, const string& suf, int k) {
+            char ch = suf[k];
+            if (node->nexts[c - 'a'] == NULL) {
+                return 0;
+            } else {
+                if (k == 0) {
+                    return node->nexts[c-'a']->state == 2 ? 2 : 1;
+                } else {
+                    return _exist(node->nexts[c-'a'], suf, k - 1);
+                }
+            }
         }
     };
-
 };
+
+/*
+ * 我的版本虽然可以AC，但是还是超时了。
+ * Leetcode的官方解答非常简洁，省去了很多重复的匹配。
+ * 以及，构建一个字典树其实是很没有必要的
+ * 哎，伤心
+ */
+
+class Solution {
+private:
+    unordered_map<int, vector<string>> ans;
+    unordered_set<string> wordSet;
+
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        wordSet = unordered_set(wordDict.begin(), wordDict.end());
+        backtrack(s, 0);
+        return ans[0];
+    }
+
+    void backtrack(const string& s, int index) {
+        if (!ans.count(index)) {
+            if (index == s.size()) {
+                ans[index] = {""};
+                return;
+            }
+            ans[index] = {};
+            for (int i = index + 1; i <= s.size(); ++i) {
+                string word = s.substr(index, i - index);
+                if (wordSet.count(word)) {
+                    backtrack(s, i);
+                    for (const string& succ: ans[i]) {
+                        ans[index].push_back(succ.empty() ? word : word + " " + succ);
+                    }
+                }
+            }
+        }
+    }
+};
+
